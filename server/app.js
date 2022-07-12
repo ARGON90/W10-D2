42f05840950cefd1d108bfb23bcd88e6c4ab1226
Lain On Pluto <lain@PLUTO-2.localdomain>
2022-07-12 14:27:41 -0700

    First part

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+app.use(express.json());
+require("express-async-errors");
 
+app.use("/static/whatever", express.static("assets"));
 // For testing purposes, GET /
-app.get('/', (req, res) => {
-  res.json("Express server running. No content provided at root level. Please use another route.");
+app.get("/", (req, res) => {
+  res.json(
+    "Express server running. No content provided at root level. Please use another route."
+  );
 });
 
 // For testing express.json middleware
-app.post('/test-json', (req, res, next) => {
+app.post("/test-json", (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
   res.json(req.body);
@@ -15,9 +20,9 @@ app.post('/test-json', (req, res, next) => {
 });
 
 // For testing express-async-errors
-app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+app.get("/test-error", async (req, res) => {
+  throw new Error("Hello World!");
 });
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
